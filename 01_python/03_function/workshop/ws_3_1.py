f43cc8ffcf8be6a8590d2aff27935f5217049094
고유한 <kyhan1563@naver.com>
2023-07-21 17:18:21 +0900

    230721

--- 01_python/03_function/workshop/ws_3_1.py
+++ 01_python/03_function/workshop/ws_3_1.py
@@ -2,7 +2,7 @@ number_of_people = 0
 
 
 def increase_user():
-    # global number_of_people
+    global number_of_people
     number_of_people += 1
     #global number_of_people 안 칠경우 global nuimber_of_people 에 +1이 불가함. local 변수가 할당되지 않았다고 뜸.
 
